e9bc759a08debe294181ef86a5842bcc39b42cd3
Martin Arce <martin.arce.teixeira@gmail.com>
2023-10-27 21:26:53 +0200

    Chiki

--- lua/chiki/keymaps.lua
+++ lua/chiki/keymaps.lua
@@ -3,12 +3,26 @@ local default_opts = { noremap = true, silent = true }
 local expr_opts = { noremap = true, expr = true, silent = true }
 
 vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
+-- keymap('n', '<Leader>fe', '<Cmd>Neotree toggle<CR>', default_opts)
 
 -- Better escape using jk in insert and terminal mode
 keymap("i", "jk", "<ESC>", default_opts)
 -- keymap("t", "jk", "<C-\\><C-n>", default_opts)
 
+
 -- Switch buffer
 keymap("n", "<S-h>", ":bprevious<CR>", default_opts)
 keymap("n", "<S-l>", ":bnext<CR>", default_opts)
+
+
+
+
+keymap('n', '<Leader>q', [[<Cmd>Neotree close<CR>:wa<CR>:q<CR>]], default_opts)
+keymap('i', '<Leader>q', [[<Esc><CR>:wa<CR>:q<CR>]], default_opts)
+keymap('n', '<Leader>w', [[:wa<CR>]], default_opts)
+
+
+
+
+
 
