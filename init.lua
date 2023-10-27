66e53b4d1d4a1d13438936d2600c8e10b87c2ea6
Martin Arce <martin.arce.teixeira@gmail.com>
2023-10-27 17:20:44 +0200

    Init

---
+++ init.lua
@@ -0,0 +1,34 @@
+print("🐕😺🐕")
+
+-- Init config
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- 
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Require Plugins in lazy
+require("lazy").setup({
+  {
+    -- Theme inspired by Atom
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'onedark'
+    end,
+  },
+
+})
+
+print("DONE init.lua");
