e01dc5f0673eae457e15a0fb5a7ffaffe2b1b981
Martin Arce <martin.arce.teixeira@gmail.com>
2023-12-07 13:07:02 +0100

    Chapter 4

---
+++ lua/options.lua
@@ -0,0 +1,35 @@
+print("options")
+
+-- Line number
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Split target
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Line wrap
+vim.opt.wrap = false
+
+-- Tab
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4 
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Scroll offset - Max will keep the scroll centered when possible
+vim.opt.scrolloff = 999
+
+-- Split when sustitute
+vim.opt.inccommand = "split" 
+
+-- Ignore case when search / :commands
+vim.opt.ignorecase = true
+
+-- 
+vim.opt.termguicolors = true
+
+
+
