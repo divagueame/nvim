d9fe6d0c197d9dc2c2163906d38c9ee2a37e98e5
Martin Arce <martin.arce.teixeira@gmail.com>
2024-01-11 10:59:02 +0100

    add volar lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -27,9 +27,6 @@ return {
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
-      lspconfig.tsserver.setup {
-        capabilities = capabilities
-      }
       lspconfig.rust_analyzer.setup {
         capabilities = capabilities
       }
@@ -39,7 +36,48 @@ return {
       lspconfig.stimulus_ls.setup{
         capabilities = capabilities
       }
+      -- lspconfig.tsserver.setup {
+      --   capabilities = capabilities
+      -- }
+      lspconfig.volar.setup{
+        capabilities = capabilities,
+        filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'},
+        init_options = {
+          typescript = {
+            tsdk = '/usr/lib/node_modules/typescript/lib'
+          }
+        }
+      }
 
+      lspconfig.ruby_ls.setup({
+        cmd = { "bundle", "exec", "ruby-lsp" },
+        on_attach = function(client, buffer)
+          local callback = function()
+            local params = vim.lsp.util.make_text_document_params(buffer)
+
+            client.request(
+              'textDocument/diagnostic',
+              { textDocument = params },
+              function(err, result)
+                if err then return end
+
+                vim.lsp.diagnostic.on_publish_diagnostics(
+                  nil,
+                  vim.tbl_extend('keep', params, { diagnostics = result.items }),
+                  { client_id = client.id }
+                )
+              end
+            )
+          end
+
+          callback() -- call on attach
+
+          vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePre', 'BufReadPost', 'InsertLeave', 'TextChanged' }, {
+            buffer = buffer,
+            callback = callback,
+          })
+        end
+      })
       -- Keymaps
       vim.keymap.set('n', '<leader>ek', vim.lsp.buf.hover, { desc = "LSP - Hover info"})
       vim.keymap.set('n', '<leader>em', vim.lsp.buf.definition, { desc = "LSP - Definition"})
