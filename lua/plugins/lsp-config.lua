a4c3f11631ead13ffc946451b3cf96a13e260e2f
Martin Arce <martin.arce.teixeira@gmail.com>
2024-02-10 23:18:24 +0100

    Add tailwind support

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -24,6 +24,7 @@ return {
       -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
+      lspconfig.tailwindcss.setup{}
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,74 +1,105 @@
 return {
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  dependencies = {
-    "hrsh7th/cmp-buffer", -- source for text in buffer
-    "hrsh7th/cmp-path", -- source for file system paths
-    "L3MON4D3/LuaSnip", -- snippet engine
-    "saadparwaiz1/cmp_luasnip", -- for autocompletion
-    "rafamadriz/friendly-snippets", -- useful snippets
-    "onsails/lspkind.nvim", -- vs-code like pictograms
-  },
-  config = function()
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-buffer", -- source for text in buffer
+      "hrsh7th/cmp-path", -- source for file system paths
+      "L3MON4D3/LuaSnip", -- snippet engine
+      "saadparwaiz1/cmp_luasnip", -- for autocompletion
+      "rafamadriz/friendly-snippets", -- useful snippets
+      "onsails/lspkind.nvim", -- vs-code like pictograms
+    },
+    config = function()
       local cmp = require("cmp")
       local luasnip = require("luasnip")
-      
-        local lspkind = require("lspkind")
+      -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+      require("luasnip.loaders.from_vscode").lazy_load()
 
-    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-    require("luasnip.loaders.from_vscode").lazy_load()
-
-    cmp.setup({
-      completion = {
-        completeopt = "menu,menuone,preview,noselect",
-      },
-      snippet = { -- configure how nvim-cmp interacts with snippet engine
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      mapping = cmp.mapping.preset.insert {
+      cmp.setup({
+        completion = {
+          completeopt = "menu,menuone,preview,noselect",
+        },
+        snippet = { -- configure how nvim-cmp interacts with snippet engine
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert {
           ['<C-d>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete {},
           ['<CR>'] = cmp.mapping.confirm {
-              behavior = cmp.ConfirmBehavior.Replace,
-              select = true,
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
           },
           ['<Tab>'] = cmp.mapping(function(fallback)
-              if cmp.visible() then
-                  cmp.select_next_item()
-              elseif luasnip.expand_or_jumpable() then
-                  luasnip.expand_or_jump()
-              else
-                  fallback()
-              end
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
           end, { 'i', 's' }),
           ['<S-Tab>'] = cmp.mapping(function(fallback)
-              if cmp.visible() then
-                  cmp.select_prev_item()
-              elseif luasnip.jumpable(-1) then
-                  luasnip.jump(-1)
-              else
-                  fallback()
-              end
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
           end, { 'i', 's' }),
-      },
-      -- sources for autocompletion
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "luasnip" }, -- snippets
-        { name = "buffer" }, -- text within current buffer
-        { name = "path" }, -- file system paths
-      }),
-      -- configure lspkind for vs-code like pictograms in completion menu
-      formatting = {
-        format = lspkind.cmp_format({
-          maxwidth = 50,
-          ellipsis_char = "...",
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        -- sources for autocompletion
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" }, -- snippets
+          { name = "buffer" }, -- text within current buffer
+          { name = "path" }, -- file system paths
         }),
-      },
-    })
-  end
+        -- configure lspkind for vs-code like pictograms in completion menu
+        formatting = {
+          fields = {'abbr', 'kind', 'menu'},
+          format = function(entry, item)
+            -- Apply lspkind formatting
+            local lspkind_format = require("lspkind").cmp_format({
+              maxwidth = 50,
+              ellipsis_char = "...",
+            })
+            lspkind_format(entry, item)
+
+            --Tailwind colorized
+            require("tailwindcss-colorizer-cmp").formatter(entry, item)
+
+            -- Source Icons
+            local menu_icon = {
+              nvim_lsp = '[🍕]',
+              luasnip = '-> SNIP',
+              buffer = '+',
+              PATH = '[ ⤴⤴⤴ ⤴  ]',
+            }
+            item.menu = menu_icon[entry.source.name]
+
+            return item
+          end
+        },
+      })
+    end
+  },
+  {
+    "roobert/tailwindcss-colorizer-cmp.nvim",
+    -- optionally, override the default options:
+    config = function()
+      require("tailwindcss-colorizer-cmp").setup({
+        color_square_width = 2,
+      })
+    end
+  }
 }
 
