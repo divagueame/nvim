a33e83ade96e23489967b62892704b734d406ec1
Martin Arce <martin.arce.teixeira@gmail.com>
2024-02-23 20:47:06 +0100

    update utils

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -1,17 +1,12 @@
 function HighlightCurrentWord()
   local cursor_word = vim.fn.expand("<cword>")
 
-  vim.cmd("nohlsearch")
+  -- vim.cmd("nohlsearch")
   vim.cmd("match Search /" .. cursor_word .. "/")
 
-  local bufnr = vim.fn.bufnr("%")
-
-  local timer = vim.loop.new_timer()
-  timer:start(1000, 0, vim.schedule_wrap(function()
-    vim.api.nvim_buf_clear_highlight(bufnr, -1, 1, -1)
-    timer:close()
-  end))
-
+  vim.defer_fn(function()
+    vim.cmd("match NONE")
+  end, 1500)
 end
 
 
