51de64d4d0b1c4df7d4c3c3e42bca5f9cc9cbad6
Martin Arce <martin.arce.teixeira@gmail.com>
2024-02-24 23:05:44 +0100

    Update utils

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -1,5 +1,6 @@
 -- HighlightCurrentWord
 function HighlightCurrentWord()
+  console.log();
   local cursor_word = vim.fn.expand("<cword>")
   vim.cmd("match Search /" .. cursor_word .. "/")
 
@@ -15,7 +16,7 @@ function InsertConsoleLog()
   local save_cursor = vim.api.nvim_win_get_cursor(0)
   vim.api.nvim_command("normal! oconsole.log();")
   vim.api.nvim_command("startinsert!")
-  vim.api.nvim_win_set_cursor(0, {save_cursor[1]+1, 14})
+  vim.api.nvim_win_set_cursor(0, {save_cursor[1]+1, 18})
 end
 vim.api.nvim_set_keymap('n', '<leader>cl', ':lua InsertConsoleLog()<CR>', {noremap = true, silent = true})
 
