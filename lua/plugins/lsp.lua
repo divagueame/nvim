df892b3a5b7a7e9e87723cd956bfdfeb4f96d794
Martin Arce <martin.arce.teixeira@gmail.com>
2024-07-12 00:10:48 +0200

    Add ruby_lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,16 +15,15 @@ return {
         ensure_installed = {
           "lua_ls",
           "rust_analyzer",
+          "ruby_lsp",
           "tsserver",
         },
       })
     end,
   },
-  -- nvim-lspconfig
   {
     "neovim/nvim-lspconfig",
     config = function()
-      -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
       lspconfig.tailwindcss.setup({})
@@ -45,49 +44,20 @@ return {
         capabilities = capabilities,
       })
       lspconfig.gopls.setup({})
+      lspconfig.ruby_lsp.setup({})
       -- lspconfig.tsserver.setup {
       --   capabilities = capabilities
       -- }
-      lspconfig.volar.setup({
-        capabilities = capabilities,
-        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
-        init_options = {
-          typescript = {
-            tsdk = "/usr/lib/node_modules/typescript/lib",
-          },
-        },
-      })
-
-      -- lspconfig.ruby_lsp.setup({
-      -- 	cmd = { "bundle", "exec", "ruby-lsp" },
-      -- 	on_attach = function(client, buffer)
-      -- 		local callback = function()
-      -- 			local params = vim.lsp.util.make_text_document_params(buffer)
-      --
-      -- 			client.request("textDocument/diagnostic", { textDocument = params }, function(err, result)
-      -- 				if err then
-      -- 					return
-      -- 				end
-      --
-      -- 				vim.lsp.diagnostic.on_publish_diagnostics(
-      -- 					nil,
-      -- 					vim.tbl_extend("keep", params, { diagnostics = result.items }),
-      -- 					{ client_id = client.id }
-      -- 				)
-      -- 			end)
-      -- 		end
-      --
-      -- 		callback() -- call on attach
-      --
-      -- 		vim.api.nvim_create_autocmd(
-      -- 			{ "BufEnter", "BufWritePre", "BufReadPost", "InsertLeave", "TextChanged" },
-      -- 			{
-      -- 				buffer = buffer,
-      -- 				callback = callback,
-      -- 			}
-      -- 		)
-      -- 	end,
+      -- lspconfig.volar.setup({
+      --   capabilities = capabilities,
+      --   filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+      --   init_options = {
+      --     typescript = {
+      --       tsdk = "/usr/lib/node_modules/typescript/lib",
+      --     },
+      --   },
       -- })
+
       -- Keymaps
       vim.keymap.set("n", "<leader>ek", vim.lsp.buf.hover, { desc = "LSP - Hover info" })
       vim.keymap.set("n", "<leader>em", vim.lsp.buf.definition, { desc = "LSP - Definition" })
@@ -97,7 +67,7 @@ return {
       -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
       -- Setup required for ufo
       local capabilities =
-          require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+      require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
       capabilities.textDocument.foldingRange = {
         dynamicRegistration = false,
         lineFoldingOnly = true,
