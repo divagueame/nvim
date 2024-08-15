fe60bb24d7d5d13ab0510e97a4c6a14cbce03e3b
Martin Arce <martin.arce.teixeira@gmail.com>
2024-08-15 23:05:54 +0200

    Fix LSP

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,109 +1,115 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        automatic_installation = true,
-        ensure_installed = {
-          "lua_ls",
-          "rust_analyzer",
-          "ruby_lsp",
-          -- "tsserver",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig_status, lspconfig = pcall(require, "lspconfig")
-      if not lspconfig_status then
-        return
-      end
+	-- Mason
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-      local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-      if not cmp_nvim_lsp_status then
-        return
-      end
+	-- Mason lspconfig
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				automatic_installation = true,
+				ensure_installed = {
+					"lua_ls",
+					"rust_analyzer",
+					"ruby_lsp",
+					-- "tsserver",
+					"solargraph",
+					"gopls",
+					"phpactor",
+					"tailwindcss",
+				},
+			})
+		end,
+	},
 
-      local capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
-      capabilities.textDocument.foldingRange = {
-        dynamicRegistration = false,
-        lineFoldingOnly = true,
-      }
+	-- LSP Configuration
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"pmizio/typescript-tools.nvim",
+		},
+		config = function()
+			local lspconfig_status, lspconfig = pcall(require, "lspconfig")
+			if not lspconfig_status then
+				return
+			end
 
-      local on_attach = function(client, bufnr)
-        local opts = { noremap = true, silent = true, buffer = bufnr }
-        vim.keymap.set(
-          "n",
-          "<leader>nm",
-          vim.lsp.buf.hover,
-          vim.tbl_extend("force", opts, { desc = "LSP - Hover info" })
-        )
-        vim.keymap.set(
-          "n",
-          "<leader>em",
-          vim.lsp.buf.definition,
-          vim.tbl_extend("force", opts, { desc = "LSP - Definition" })
-        )
-        vim.keymap.set(
-          "n",
-          "<leader>e,",
-          vim.lsp.buf.references,
-          vim.tbl_extend("force", opts, { desc = "LSP - References" })
-        )
-        vim.keymap.set(
-          "n",
-          "<leader>e.",
-          vim.lsp.buf.code_action,
-          vim.tbl_extend("force", opts, { desc = "LSP - Code action" })
-        )
-      end
+			local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+			if not cmp_nvim_lsp_status then
+				return
+			end
 
-      local servers = {
-        "lua_ls",
-        "rust_analyzer",
-        "ruby_lsp",
-        "solargraph",
-        "gopls",
-        "phpactor",
-      }
+			-- Capabilities
+			local capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+			capabilities.textDocument.foldingRange = {
+				dynamicRegistration = false,
+				lineFoldingOnly = true,
+			}
 
-      for _, server in ipairs(servers) do
-        lspconfig[server].setup({
-          capabilities = capabilities,
-          on_attach = on_attach,
-        })
-      end
+			-- On attach function
+			local on_attach = function(_client, bufnr)
+				local opts = { noremap = true, silent = true, buffer = bufnr }
+				vim.keymap.set(
+					"n",
+					"<leader>jk",
+					vim.lsp.buf.hover,
+					vim.tbl_extend("force", opts, { desc = "LSP - Hover info" })
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>jj",
+					vim.lsp.buf.definition,
+					vim.tbl_extend("force", opts, { desc = "LSP - Definition" })
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>jl",
+					"<cmd>Telescope lsp_references<cr>",
+					vim.tbl_extend("force", opts, { desc = "LSP - References (Telescope)" })
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>j<CR>",
+					vim.lsp.buf.code_action,
+					vim.tbl_extend("force", opts, { desc = "LSP - Code action" })
+				)
+			end
 
-      lspconfig.tailwindcss.setup({})
-      lspconfig.stimulus_ls.setup({
-        cmd = { "node", "/home/ma/web/source/stimulus-lsp/server/out/server.js", "--stdio" },
-        capabilities = capabilities,
-      })
-      -- lspconfig.tsserver.setup {
-      --   capabilities = capabilities
-      -- }
-      -- lspconfig.volar.setup({
-      -- 	capabilities = capabilities,
-      -- 	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
-      -- 	init_options = {
-      -- 		typescript = {
-      -- 			tsdk = "/usr/lib/node_modules/typescript/lib",
-      -- 		},
-      -- 	},
-      -- })
-    end,
-  },
-  {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {},
-  },
+			local servers = {
+				"lua_ls",
+				"rust_analyzer",
+				"ruby_lsp",
+				"solargraph",
+				"gopls",
+				"phpactor",
+				"tailwindcss",
+			}
+
+			for _, server in ipairs(servers) do
+				lspconfig[server].setup({
+					capabilities = capabilities,
+					on_attach = on_attach,
+				})
+			end
+
+			-- Special configurations
+			lspconfig.stimulus_ls.setup({
+				cmd = { "node", "/home/ma/web/source/stimulus-lsp/server/out/server.js", "--stdio" },
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+
+			-- TypeScript setup
+			require("typescript-tools").setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+		end,
+	},
 }
