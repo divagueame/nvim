4494007762e33aacf15c838c9a611aaae0d8b3be
Martin Arce <martin.arce.teixeira@gmail.com>
2024-08-29 10:24:07 +0200

    Add prettier to format markdon

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,1 +1,16 @@
-vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = { "*.md" },
+	callback = function()
+		vim.fn.system("prettier --write " .. vim.fn.expand("%"))
+		vim.cmd("edit")
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function()
+		if vim.bo.filetype ~= "markdown" then
+			vim.lsp.buf.format()
+		end
+	end,
+})
