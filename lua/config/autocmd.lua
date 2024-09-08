ab8415158161719dc8a8011b9bd8a8917c29e5e1
Martin Arce <martin.arce.teixeira@gmail.com>
2024-09-08 19:48:49 +0200

    Auto com

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,18 +1,19 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = { "*.md" },
-	callback = function()
-		vim.fn.system("prettier --write " .. vim.fn.expand("%"))
-		vim.cmd("edit")
-	end,
+  pattern = { "*.md" },
+  callback = function()
+    vim.fn.system("prettier --write " .. vim.fn.expand("%"))
+    vim.cmd("edit")
+  end,
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function()
-		if vim.bo.filetype ~= "markdown" then
-			vim.lsp.buf.format()
-		end
-	end,
+  pattern = "*",
+  callback = function()
+    if vim.bo.filetype ~= "markdown" then
+      vim.lsp.buf.format()
+    end
+  end,
+})
 
 local yank_group = vim.api.nvim_create_augroup("highlight_yank", { clear = true })
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
