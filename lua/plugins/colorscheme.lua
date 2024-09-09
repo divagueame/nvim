ef75af1550090fc3a73fb17fc2f21adbc8c03a05
Martin Arce <martin.arce.teixeira@gmail.com>
2024-09-09 22:11:21 +0200

    Update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,16 @@
+-- return {
+-- 	"rebelot/kanagawa.nvim",
+-- 	config = function()
+-- 		-- vim.cmd.colorscheme("kanagawa-wave")
+-- 		vim.cmd.colorscheme("kanagawa-dragon")
+-- 	end,
+-- }
 return {
-	"rebelot/kanagawa.nvim",
+	"sainnhe/gruvbox-material",
+	lazy = false,
+	priority = 1000,
 	config = function()
-		-- vim.cmd.colorscheme("kanagawa-wave")
-		vim.cmd.colorscheme("kanagawa-dragon")
+		vim.g.gruvbox_material_enable_italic = true
+		vim.cmd.colorscheme("gruvbox-material")
 	end,
 }
