9dd1748e923ccdabb6bc006fb100d93e21aefead
Martin Arce <martin.arce.teixeira@gmail.com>
2024-09-18 21:51:00 +0200

    Update cmp + Lua snipppter setup

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,116 +1,108 @@
 return {
-	{
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = {
-			"hrsh7th/cmp-buffer", -- source for text in buffer
-			"hrsh7th/cmp-path", -- source for file system paths
-			"hrsh7th/cmp-cmdline",
-			"L3MON4D3/LuaSnip", -- snippet engine
-			"saadparwaiz1/cmp_luasnip", -- for autocompletion
-			"rafamadriz/friendly-snippets", -- useful snippets
-			"onsails/lspkind.nvim", -- vs-code like pictograms
-			{ "hrsh7th/nvim-cmp" },
-			{ "hrsh7th/cmp-vsnip" },
-			{ "hrsh7th/vim-vsnip" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-		},
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+      "onsails/lspkind.nvim",
+      'windwp/nvim-autopairs',
+      { "hrsh7th/cmp-nvim-lsp" },
+    },
+    config = function()
+      local cmp = require("cmp")
+      local ls = require("luasnip")
 
-		config = function()
-			local cmp = require("cmp")
-			local ls = require("luasnip")
+      require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets" })
+      require("luasnip.loaders.from_vscode").lazy_load()
 
-			require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets" })
-			-- Autopairs config
-			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
-			cmp.setup({
-				completion = {
-					completeopt = "menu,menuone,preview,noselect",
-				},
+      cmp.setup({
+        completion = {
+          completeopt = "menu,menuone,preview,noselect",
+        },
 
-				snippet = { -- configure how nvim-cmp interacts with snippet engine
-					expand = function(args)
-						ls.lsp_expand(args.body)
-					end,
-				},
+        snippet = {
+          expand = function(args)
+            ls.lsp_expand(args.body)
+          end,
+        },
 
-				mapping = cmp.mapping.preset.insert({
-					["<C-d>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete({}),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({
-						behavior = cmp.ConfirmBehavior.Replace,
-						select = true,
-					}),
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif ls.expand_or_jumpable() then
-							ls.expand_or_jump()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif ls.jumpable(-1) then
-							ls.jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-				}),
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				-- sources for autocompletion
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- snippets
-					{ name = "buffer" }, -- text within current buffer
-					{ name = "path" }, -- file system paths
-				}),
-				-- configure lspkind for vs-code like pictograms in completion menu
-				formatting = {
-					fields = { "abbr", "kind", "menu" },
-					format = function(entry, item)
-						-- Apply lspkind formatting
-						local lspkind_format = require("lspkind").cmp_format({
-							maxwidth = 50,
-							ellipsis_char = "...",
-						})
-						lspkind_format(entry, item)
+        mapping = cmp.mapping.preset.insert({
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete({}),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif ls.expand_or_jumpable() then
+              ls.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif ls.jumpable(-1) then
+              ls.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp", priority = 1000 },
+          { name = "luasnip", priority = 750 },
+          { name = "buffer", priority = 500 },
+          { name = "path", priority = 250 },				}),
+        formatting = {
+          fields = { "abbr", "kind", "menu" },
+          format = function(entry, item)
+            -- Apply lspkind formatting
+            local lspkind_format = require("lspkind").cmp_format({
+              maxwidth = 50,
+              ellipsis_char = "...",
+            })
+            lspkind_format(entry, item)
 
-						--Tailwind colorized
-						require("tailwindcss-colorizer-cmp").formatter(entry, item)
+            require("tailwindcss-colorizer-cmp").formatter(entry, item)
 
-						-- Source Icons
-						local menu_icon = {
-							nvim_lsp = "[🍕]",
-							luasnip = "-> SNIP",
-							buffer = "+",
-							PATH = "[ ⤴⤴⤴ ⤴  ]",
-						}
-						item.menu = menu_icon[entry.source.name]
+            local menu_icon = {
+              nvim_lsp = "[🍕]",
+              luasnip = "-> SNIP",
+              buffer = "+",
+              PATH = "[ ⤴⤴⤴ ⤴  ]",
+            }
+            item.menu = menu_icon[entry.source.name]
 
-						return item
-					end,
-				},
-			})
-		end,
-	},
-	{
-		"roobert/tailwindcss-colorizer-cmp.nvim",
-		-- optionally, override the default options:
-		config = function()
-			require("tailwindcss-colorizer-cmp").setup({
-				color_square_width = 2,
-			})
-		end,
-	},
+            return item
+          end,
+        },
+      })
+    end,
+  },
+  {
+    "roobert/tailwindcss-colorizer-cmp.nvim",
+    config = function()
+      require("tailwindcss-colorizer-cmp").setup({
+        color_square_width = 2,
+      })
+    end,
+  },
 }
---
+++ lua/snippets/lua.lua
@@ -0,0 +1,88 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+return {
+  -- Function snippet
+  s("fn", fmt(
+    [[
+    functionmeow {}({})
+      {}
+    end
+    ]],
+    {
+      i(1, "name"),
+      i(2, "args"),
+      i(3, "-- TODO: implement")
+    }
+  )),
+
+  -- Require snippet
+  s("req", fmt(
+    [[local {} = require("{}")]]
+    ,
+    {
+      f(function(import_name)
+        local parts = vim.split(import_name[1][1], ".", true)
+        return parts[#parts] or ""
+      end, {1}),
+      i(1, "module_name")
+    }
+  )),
+
+  -- For loop snippet
+  s("for", fmt(
+    [[
+    for {} = {}, {} do
+      {}
+    end
+    ]],
+    {
+      i(1, "i"),
+      i(2, "1"),
+      i(3, "10"),
+      i(4, "-- TODO: implement")
+    }
+  )),
+
+  -- If statement snippet
+  s("if", fmt(
+    [[
+    if {} then
+      {}
+    end
+    ]],
+    {
+      i(1, "condition"),
+      i(2, "-- TODO: implement")
+    }
+  )),
+
+  -- Neovim plugin spec snippet
+  s("plug", fmt(
+    [[
+    {{
+      "{}",
+      {}
+      config = function()
+        {}
+      end,
+    }},
+    ]],
+    {
+      i(1, "author/plugin-name"),
+      c(2, {
+        t(""),
+        fmt([[dependencies = {{ "{}" }},]], { i(1, "dependency") }),
+        t('event = "VeryLazy",'),
+        t('cmd = "",'),
+      }),
+      i(3, "-- TODO: configure plugin")
+    }
+  )),
+}
