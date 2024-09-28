cb2ee7db8cdfdb46970035123615e40fcbcf832b
Martin Arce <martin.arce.teixeira@gmail.com>
2024-09-28 21:07:18 +0200

    Clean autocmd

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,38 +1,25 @@
--- vim.api.nvim_create_autocmd("BufWritePre", {
--- 	pattern = { "*.md" },
--- 	callback = function()
--- 		vim.fn.system("prettier --write " .. vim.fn.expand("%"))
--- 		vim.cmd("edit")
--- 	end,
--- })
-
--- vim.api.nvim_create_autocmd("BufWritePre", {
--- 	pattern = "*",
--- 	callback = function()
--- 		if vim.bo.filetype ~= "markdown" then
--- 			vim.lsp.buf.format()
--- 		end
--- 	end,
--- })
-
--- Highlight Yank
+--- Highlight Yank
 local yank_group = vim.api.nvim_create_augroup("highlight_yank", { clear = true })
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-  pattern = "*",
-  group = yank_group,
-  callback = function()
-    vim.highlight.on_yank({ higroup = "YankColor", timeout = 200 })
-  end,
+	pattern = "*",
+	group = yank_group,
+	callback = function()
+		vim.highlight.on_yank({ higroup = "YankColor", timeout = 200 })
+	end,
 })
 
 -- Autosave
-vim.api.nvim_create_autocmd(
-  {
-    "FocusLost",
-    "BufLeave",
-    -- "ModeChanged",
-    -- "TextChanged",
-    -- "BufEnter"
-  },
-  { desc = "autosave", pattern = "*", command = "silent! update" }
-)
+vim.api.nvim_create_autocmd({
+	"FocusLost",
+	"BufLeave",
+	-- "ModeChanged",
+	-- "TextChanged",
+	-- "BufEnter"
+}, { desc = "autosave", pattern = "*", command = "silent! update" })
+
+-- Toggle line wrapping
+vim.api.nvim_set_keymap("n", "<leader>t", ":set wrap!<CR>", { noremap = true, silent = true })
+vim.api.nvim_create_user_command("ToggleWrap", function()
+	vim.wo.wrap = not vim.wo.wrap
+	print("Line wrap is now " .. (vim.wo.wrap and "on" or "off"))
+end, {})
