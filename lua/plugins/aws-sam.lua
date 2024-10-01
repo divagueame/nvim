1ba1e9a5329e6f838f45dc9dca74444018d4eb9b
Martin Arce <martin.arce.teixeira@gmail.com>
2024-10-01 20:44:50 +0200

    Plugins for dev

--- lua/plugins/aws-sam.lua
+++ lua/plugins/aws-sam.lua
@@ -1,17 +1,30 @@
 -- return {}
 return {
-  -- "divagueame/aws-sam.nvim",
-  dir = os.getenv("HOME") .. "/web/plugins/aws-sam.nvim",
-  dependencies = {
-    {
-      "rcarriga/nvim-notify",
-    }
+  {
+    -- "divagueame/lacasitos.nvim",
+    dir = os.getenv("HOME") .. "/web/plugins/lacasitos.nvim",
+    -- event = "BufReadPost",
+    dev = true,
+    config = function()
+      require("lacasitos").setup({})
+      print("Loading lacasitos plugin...")
+    end,
   },
-  -- event = "BufReadPost",
-  dev = true,
-  config = function()
-    require("aws-sam").setup({
-      keymaps = true,
-    })
-  end,
+  {
+    -- "divagueame/aws-sam.nvim",
+    dir = os.getenv("HOME") .. "/web/plugins/aws-sam.nvim",
+    dependencies = {
+      {
+        "rcarriga/nvim-notify",
+        "divagueame/lacasitos.nvim",
+      }
+    },
+    -- event = "BufReadPost",
+    dev = true,
+    config = function()
+      require("aws-sam").setup({
+        keymaps = true,
+      })
+    end,
+  }
 }
