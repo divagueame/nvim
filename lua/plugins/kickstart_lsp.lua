bdb4fecaee5dca67a9fe82b3a51d49bf5503dfbc
Martin Arce <martin.arce.teixeira@gmail.com>
2024-11-09 08:07:39 +0100

    add kisktast lsp

---
+++ lua/plugins/kickstart_lsp.lua
@@ -0,0 +1,429 @@
+-- return {}
+return {
+	-- LSP Plugins
+	{
+		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+		-- used for completion, annotations and signatures of Neovim apis
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+	{ "Bilal2453/luvit-meta", lazy = true },
+	{
+		-- Main LSP Configuration
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- Automatically install LSPs and related tools to stdpath for Neovim
+			{ "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+
+			-- Useful status updates for LSP.
+			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+			{ "j-hui/fidget.nvim", opts = {} },
+
+			-- Allows extra capabilities provided by nvim-cmp
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function()
+			--  This function gets run when an LSP attaches to a particular buffer.
+			--    That is to say, every time a new file is opened that is associated with
+			--    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
+			--    function will be executed to configure the current buffer
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+				callback = function(event)
+					-- NOTE: Remember that Lua is a real programming language, and as such it is possible
+					-- to define small helper and utility functions so you don't have to repeat yourself.
+					--
+					-- In this case, we create a function that lets us more easily define mappings specific
+					-- for LSP related items. It sets the mode, buffer and description for us each time.
+					local map = function(keys, func, desc, mode)
+						mode = mode or "n"
+						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					end
+
+					-- Jump to the definition of the word under your cursor.
+					--  This is where a variable was first declared, or where a function is defined, etc.
+					--  To jump back, press <C-t>.
+					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+
+					map("gK", vim.lsp.buf.hover, "HOVER LSP")
+					--
+					-- Find references for the word under your cursor.
+					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+
+					-- Jump to the implementation of the word under your cursor.
+					--  Useful when your language has ways of declaring types without an actual implementation.
+					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+
+					-- Jump to the type of the word under your cursor.
+					--  Useful when you're not sure what type a variable is and you want to see
+					--  the definition of its *type*, not where it was *defined*.
+					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+
+					-- Fuzzy find all the symbols in your current document.
+					--  Symbols are things like variables, functions, types, etc.
+					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+
+					-- Fuzzy find all the symbols in your current workspace.
+					--  Similar to document symbols, except searches over your entire project.
+					map(
+						"<leader>ws",
+						require("telescope.builtin").lsp_dynamic_workspace_symbols,
+						"[W]orkspace [S]ymbols"
+					)
+
+					-- Rename the variable under your cursor.
+					--  Most Language Servers support renaming across files, etc.
+					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+
+					-- Execute a code action, usually your cursor needs to be on top of an error
+					-- or a suggestion from your LSP for this to activate.
+					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
+
+					-- WARN: This is not Goto Definition, this is Goto Declaration.
+					--  For example, in C this would take you to the header.
+					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+					map("<leader>m", function()
+						vim.lsp.buf.format({ async = true })
+						vim.cmd("write")
+					end, "[G]oto [D]eclaration")
+
+					-- The following two autocommands are used to highlight references of the
+					-- word under your cursor when your cursor rests there for a little while.
+					--    See `:help CursorHold` for information about when this is executed
+					--
+					-- When you move your cursor, the highlights will be cleared (the second autocommand).
+					local client = vim.lsp.get_client_by_id(event.data.client_id)
+					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+						local highlight_augroup =
+							vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+							buffer = event.buf,
+							group = highlight_augroup,
+							callback = vim.lsp.buf.document_highlight,
+						})
+
+						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+							buffer = event.buf,
+							group = highlight_augroup,
+							callback = vim.lsp.buf.clear_references,
+						})
+
+						vim.api.nvim_create_autocmd("LspDetach", {
+							group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+							callback = function(event2)
+								vim.lsp.buf.clear_references()
+								vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+							end,
+						})
+					end
+
+					-- The following code creates a keymap to toggle inlay hints in your
+					-- code, if the language server you are using supports them
+					--
+					-- This may be unwanted, since they displace some of your code
+					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+						map("<leader>th", function()
+							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+						end, "[T]oggle Inlay [H]ints")
+					end
+				end,
+			})
+
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities.textDocument.foldingRange = {
+				dynamicRegistration = false,
+				lineFoldingOnly = true,
+			}
+			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+			local border = {
+				{ "┌", "FloatBorder" },
+				{ "─", "FloatBorder" },
+				{ "┐", "FloatBorder" },
+				{ "│", "FloatBorder" },
+				{ "┘", "FloatBorder" },
+				{ "─", "FloatBorder" },
+				{ "└", "FloatBorder" },
+				{ "│", "FloatBorder" },
+			}
+
+			local handlers = {
+				["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+					border = border,
+
+					width = 800,
+				}),
+				["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+					border = border,
+					width = 80,
+				}),
+			}
+			local mason_registry = require("mason-registry")
+			local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+				.. "/node_modules/@vue/language-server"
+
+			local servers = {
+				-- gopls = {},
+				-- pyright = {},
+				-- rust_analyzer = {},
+				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+				ts_ls = {
+					handlers = handlers,
+					filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+					init_options = {
+						plugins = {
+							{
+								name = "@vue/typescript-plugin",
+								location = vue_language_server_path,
+								languages = { "vue" },
+							},
+						},
+					},
+				},
+				lua_ls = {
+					-- cmd = {...},
+					-- filetypes = { ...},
+					-- capabilities = {},
+					settings = {
+						Lua = {
+							completion = {
+								callSnippet = "Replace",
+							},
+							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+							-- diagnostics = { disable = { 'missing-fields' } },
+						},
+					},
+				},
+			}
+
+			-- Ensure the servers and tools above are installed
+			--  To check the current status of installed tools and/or manually install
+			--  other tools, you can run
+			--    :Mason
+			--
+			--  You can press `g?` for help in this menu.
+			require("mason").setup()
+
+			-- You can add other tools here that you want Mason to install
+			-- for you, so that they are available from within Neovim.
+			local ensure_installed = vim.tbl_keys(servers or {})
+			vim.list_extend(ensure_installed, {
+				"stylua", -- Used to format Lua code
+			})
+			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+			require("mason-lspconfig").setup({
+				handlers = {
+					function(server_name)
+						local server = servers[server_name] or {}
+						-- This handles overriding only values explicitly passed
+						-- by the server configuration above. Useful when disabling
+						-- certain features of an LSP (for example, turning off formatting for ts_ls)
+						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+						require("lspconfig")[server_name].setup(server)
+					end,
+				},
+			})
+		end,
+	},
+
+	-- { -- Autoformat
+	-- 	"stevearc/conform.nvim",
+	-- 	event = { "BufWritePre" },
+	-- 	cmd = { "ConformInfo" },
+	-- 	keys = {
+	-- 		{
+	-- 			"<leader>[",
+	-- 			function()
+	-- 				require("conform").format({ async = true, lsp_format = "fallback" })
+	-- 			end,
+	-- 			mode = "",
+	-- 			desc = "[F]ormat buffer",
+	-- 		},
+	-- 	},
+	-- 	opts = {
+	-- 		notify_on_error = false,
+	-- 		format_on_save = function(bufnr)
+	-- 			-- Disable "format_on_save lsp_fallback" for languages that don't
+	-- 			-- have a well standardized coding style. You can add additional
+	-- 			-- languages here or re-enable it for the disabled ones.
+	-- 			local disable_filetypes = { c = true, cpp = true }
+	-- 			local lsp_format_opt
+	-- 			if disable_filetypes[vim.bo[bufnr].filetype] then
+	-- 				lsp_format_opt = "never"
+	-- 			else
+	-- 				lsp_format_opt = "fallback"
+	-- 			end
+	-- 			return {
+	-- 				timeout_ms = 500,
+	-- 				lsp_format = lsp_format_opt,
+	-- 			}
+	-- 		end,
+	-- 		formatters_by_ft = {
+	-- 			lua = { "stylua" },
+	-- 			-- Conform can also run multiple formatters sequentially
+	-- 			-- python = { "isort", "black" },
+	-- 			--
+	-- 			-- You can use 'stop_after_first' to run the first available formatter from the list
+	-- 			-- javascript = { "prettierd", "prettier", stop_after_first = true },
+	-- 		},
+	-- 	},
+	-- },
+	--   {
+	--   'saghen/blink.cmp',
+	--   lazy = false, -- lazy loading handled internally
+	--   -- optional: provides snippets for the snippet source
+	--   dependencies = 'rafamadriz/friendly-snippets',
+	--
+	--   -- use a release tag to download pre-built binaries
+	--   version = 'v0.*',
+	--   -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+	--   -- build = 'cargo build --release',
+	--   -- On musl libc based systems you need to add this flag
+	--   -- build = 'RUSTFLAGS="-C target-feature=-crt-static" cargo build --release',
+	--
+	--   opts = {
+	--     highlight = {
+	--       -- sets the fallback highlight groups to nvim-cmp's highlight groups
+	--       -- useful for when your theme doesn't support blink.cmp
+	--       -- will be removed in a future release, assuming themes add support
+	--       use_nvim_cmp_as_default = true,
+	--     },
+	--     -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+	--     -- adjusts spacing to ensure icons are aligned
+	--     nerd_font_variant = 'normal',
+	--
+	--     -- experimental auto-brackets support
+	--     -- accept = { auto_brackets = { enabled = true } }
+	--
+	--     -- experimental signature help support
+	--     -- trigger = { signature_help = { enabled = true } }
+	--   }
+	-- }
+
+	-- { -- Autocompletion
+	-- 	"hrsh7th/nvim-cmp",
+	-- 	event = "InsertEnter",
+	-- 	dependencies = {
+	-- 		-- Snippet Engine & its associated nvim-cmp source
+	-- 		{
+	-- 			"L3MON4D3/LuaSnip",
+	-- 			build = (function()
+	-- 				-- Build Step is needed for regex support in snippets.
+	-- 				-- This step is not supported in many windows environments.
+	-- 				-- Remove the below condition to re-enable on windows.
+	-- 				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+	-- 					return
+	-- 				end
+	-- 				return "make install_jsregexp"
+	-- 			end)(),
+	-- 			dependencies = {
+	-- 				-- `friendly-snippets` contains a variety of premade snippets.
+	-- 				--    See the README about individual language/framework/plugin snippets:
+	-- 				--    https://github.com/rafamadriz/friendly-snippets
+	-- 				-- {
+	-- 				--   'rafamadriz/friendly-snippets',
+	-- 				--   config = function()
+	-- 				--     require('luasnip.loaders.from_vscode').lazy_load()
+	-- 				--   end,
+	-- 				-- },
+	-- 			},
+	-- 		},
+	-- 		"saadparwaiz1/cmp_luasnip",
+	--
+	-- 		-- Adds other completion capabilities.
+	-- 		--  nvim-cmp does not ship with all sources by default. They are split
+	-- 		--  into multiple repos for maintenance purposes.
+	-- 		"hrsh7th/cmp-nvim-lsp",
+	-- 		"hrsh7th/cmp-path",
+	-- 	},
+	-- 	config = function()
+	-- 		-- See `:help cmp`
+	-- 		local cmp = require("cmp")
+	-- 		local luasnip = require("luasnip")
+	-- 		luasnip.config.setup({})
+	--
+	-- 		cmp.setup({
+	-- 			snippet = {
+	-- 				expand = function(args)
+	-- 					luasnip.lsp_expand(args.body)
+	-- 				end,
+	-- 			},
+	-- 			completion = { completeopt = "menu,menuone,noinsert" },
+	--
+	-- 			-- For an understanding of why these mappings were
+	-- 			-- chosen, you will need to read `:help ins-completion`
+	-- 			--
+	-- 			-- No, but seriously. Please read `:help ins-completion`, it is really good!
+	-- 			mapping = cmp.mapping.preset.insert({
+	-- 				-- Select the [n]ext item
+	-- 				["<C-n>"] = cmp.mapping.select_next_item(),
+	-- 				-- Select the [p]revious item
+	-- 				["<C-p>"] = cmp.mapping.select_prev_item(),
+	--
+	-- 				-- Scroll the documentation window [b]ack / [f]orward
+	-- 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+	-- 				["<C-f>"] = cmp.mapping.scroll_docs(4),
+	--
+	-- 				-- Accept ([y]es) the completion.
+	-- 				--  This will auto-import if your LSP supports it.
+	-- 				--  This will expand snippets if the LSP sent a snippet.
+	-- 				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+	--
+	-- 				-- If you prefer more traditional completion keymaps,
+	-- 				-- you can uncomment the following lines
+	-- 				['<CR>'] = cmp.mapping.confirm { select = true },
+	-- 				['<Tab>'] = cmp.mapping.select_next_item(),
+	-- 				['<S-Tab>'] = cmp.mapping.select_prev_item(),
+	--
+	-- 				-- Manually trigger a completion from nvim-cmp.
+	-- 				--  Generally you don't need this, because nvim-cmp will display
+	-- 				--  completions whenever it has completion options available.
+	-- 				["<C-Space>"] = cmp.mapping.complete({}),
+	--
+	-- 				-- Think of <c-l> as moving to the right of your snippet expansion.
+	-- 				--  So if you have a snippet that's like:
+	-- 				--  function $name($args)
+	-- 				--    $body
+	-- 				--  end
+	-- 				--
+	-- 				-- <c-l> will move you to the right of each of the expansion locations.
+	-- 				-- <c-h> is similar, except moving you backwards.
+	-- 				["<C-l>"] = cmp.mapping(function()
+	-- 					if luasnip.expand_or_locally_jumpable() then
+	-- 						luasnip.expand_or_jump()
+	-- 					end
+	-- 				end, { "i", "s" }),
+	-- 				["<C-h>"] = cmp.mapping(function()
+	-- 					if luasnip.locally_jumpable(-1) then
+	-- 						luasnip.jump(-1)
+	-- 					end
+	-- 				end, { "i", "s" }),
+	--
+	-- 				-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+	-- 				--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+	-- 			}),
+	-- 			sources = {
+	-- 				{
+	-- 					name = "lazydev",
+	-- 					-- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
+	-- 					group_index = 0,
+	-- 				},
+	-- 				{ name = "nvim_lsp" },
+	-- 				{ name = "luasnip" },
+	-- 				{ name = "path" },
+	--          { name = 'buffer' },
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
+}
