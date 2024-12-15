0281c4689ac9b6011028d2001aa73905ee4fa350
Martin Arce <martin.arce.teixeira@gmail.com>
2024-12-15 19:23:41 +0100

    fix highilght

--- lua/config/highlight_groups.lua
+++ lua/config/highlight_groups.lua
@@ -2,9 +2,9 @@ vim.api.nvim_set_hl(0, "YankColor", { fg = "#34495E", bg = "#2FCD7F" })
 -- vim.cmd("highlight NotifyBackground guifg=#edebbd guibg=#1d2021 gui=bold")
 
 --- Harpoon Panel
-vim.api.nvim_set_hl(0, 'HarpoonSelectedOptionHL', { bg = "#121212", fg = "#2FCD7F", bold = true })
-vim.api.nvim_set_hl(0, 'HarpoonOptionHL', { fg = "#f9000f", bg = "NONE", bold = true })
-vim.api.nvim_set_hl(0, 'HarpoonFilesPanelHL', { fg = "#f0f00f", bg = "NONE", bold = true })
+vim.api.nvim_set_hl(0, "HarpoonSelectedOptionHL", { bg = "#121212", fg = "#2FCD7F", bold = true })
+vim.api.nvim_set_hl(0, "HarpoonOptionHL", { fg = "#f9000f", bg = "NONE", bold = true })
+vim.api.nvim_set_hl(0, "HarpoonFilesPanelHL", { fg = "#f0f00f", bg = "NONE", bold = true })
 
 -- Customize the floating window's text and border
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e1e2e", fg = "#c0caf5" })
@@ -18,10 +18,10 @@ vim.api.nvim_set_hl(0, 'HarpoonFilesPanelHL', { fg = "#f0f00f", bg = "NONE", bol
 -- vim.api.nvim_set_hl(0, "Title", { fg = "#fabd2f", bg = "#1d2021", bold = true })  -- Gruvbox yellow on dark bg
 -- vim.api.nvim_set_hl(0, "Title", { fg = "#fabd2f", bg = "#0f3094", bold = true })  -- Gruvbox yellow on dark bg
 -- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#003093",fg = "#0f3093" })
-vim.api.nvim_set_hl(0, "NormalFloatCustom", { bg = "#003093",fg = "#0f3093" })
+vim.api.nvim_set_hl(0, "NormalFloatCustom", { bg = "#003093", fg = "#0f3093" })
 
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#003093",fg = "#dfdfdf" })
--- vim.api.nvim_set_hl(0, "FloatTitle", { bg = "#003093",fg = "#ff00cc" })
+vim.api.nvim_set_hl(0, "FloatTitle", { bg = "#afff8f", fg = "#1d2021" })
 -- vim.api.nvim_set_hl(0, "FloatFooter", { bg = "#003093",fg = "#0f3093" })
 --
 --
@@ -31,3 +31,5 @@ vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
 
 -- Link NormalFloat to Normal
 vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
+
+vim.api.nvim_set_hl(0, "MyCustomHighlight", { fg = "#FFD700", bg = "#005f87", bold = true })
