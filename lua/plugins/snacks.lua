0e9707aa31ab9b930e34965397a4397aa8d6ab69
Martin Arce <martin.arce.teixeira@gmail.com>
2024-12-15 18:28:31 +0100

    snacks remove

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -72,27 +72,27 @@ return {
 		-- 	end,
 		-- 	desc = "Git Blame Line",
 		-- },
-		{
-			"<leader>gf",
-			function()
-				Snacks.lazygit.log_file()
-			end,
-			desc = "Lazygit Current File History",
-		},
-		{
-			"<leader>gg",
-			function()
-				Snacks.lazygit()
-			end,
-			desc = "Lazygit",
-		},
-		{
-			"<leader>gl",
-			function()
-				Snacks.lazygit.log()
-			end,
-			desc = "Lazygit Log (cwd)",
-		},
+		-- {
+		-- 	"<leader>gf",
+		-- 	function()
+		-- 		Snacks.lazygit.log_file()
+		-- 	end,
+		-- 	desc = "Lazygit Current File History",
+		-- },
+		-- {
+		-- 	"<leader>gg",
+		-- 	function()
+		-- 		Snacks.lazygit()
+		-- 	end,
+		-- 	desc = "Lazygit",
+		-- },
+		-- {
+		-- 	"<leader>gl",
+		-- 	function()
+		-- 		Snacks.lazygit.log()
+		-- 	end,
+		-- 	desc = "Lazygit Log (cwd)",
+		-- },
 		{
 			"<leader>un",
 			function()
