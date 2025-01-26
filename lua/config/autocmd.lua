3878fe9da9bf7f40774c905d5e0cbf15f6449434
Martin Arce <martin.arce.teixeira@gmail.com>
2025-01-26 18:26:04 +0100

    add test currentfile shortcut

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -24,3 +24,18 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.cmd("only") -- Close all other splits
 	end,
 })
+
+vim.api.nvim_set_keymap("n", "<leader>tt", [[:lua RunTestFile()<CR>]], { noremap = true, silent = true })
+
+function RunTestFile()
+	local file = vim.fn.expand("%:t")
+	local joviva_home = os.getenv("JOVIVA_HOME")
+
+	local wezterm_command =
+		string.format("wezterm start -- bash -c 'cd %s && yarn vitest %s; exec bash'", joviva_home, file)
+
+	local i3_command = string.format("i3-msg 'workspace number 3; exec \"%s\"'", wezterm_command)
+
+	-- Execute the i3-msg command
+	os.execute(i3_command)
+end
