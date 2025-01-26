1084591a825282620ce18b553393b19745a64d21
Martin Arce <martin.arce.teixeira@gmail.com>
2025-01-26 18:07:20 +0100

    up

--- init.lua
+++ init.lua
@@ -132,20 +132,42 @@ require("lazy").setup({
 			},
 		},
 	},
-
-	-- NOTE: Plugins can also be configured to run Lua code when they are loaded.
-	--
-	-- This is often very useful to both group configuration, as well as handle
-	-- lazy loading plugins that don't need to be loaded immediately at startup.
-	--
-	-- For example, in the following configuration, we use:
-	--  event = 'VimEnter'
-	--
-	-- which loads which-key before all the UI elements are loaded. Events can be
-	-- normal autocommands events (`:help autocmd-events`).
-	--
-	-- Then, because we use the `opts` key (recommended), the configuration runs
-	-- after the plugin has been loaded as `require(MODULE).setup(opts)`.
+	{
+		"stevearc/resession.nvim",
+		opts = {},
+		config = function()
+			local resession = require("resession")
+			resession.setup({
+				autosave = {
+					enabled = true,
+					interval = 60,
+					notify = false,
+				},
+			})
+			local function get_session_name()
+				local name = vim.fn.getcwd()
+				local branch = vim.trim(vim.fn.system("git branch --show-current"))
+				if vim.v.shell_error == 0 then
+					return name .. branch
+				else
+					return name
+				end
+			end
+			vim.api.nvim_create_autocmd("VimEnter", {
+				callback = function()
+					-- Only load the session if nvim was started with no args
+					if vim.fn.argc(-1) == 0 then
+						resession.load(get_session_name(), { dir = "dirsession", silence_errors = true })
+					end
+				end,
+			})
+			vim.api.nvim_create_autocmd("VimLeavePre", {
+				callback = function()
+					resession.save(get_session_name(), { dir = "dirsession", notify = false })
+				end,
+			})
+		end,
+	},
 
 	{ -- Useful plugin to show you pending keybinds.
 		"folke/which-key.nvim",
