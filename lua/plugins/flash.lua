4dbab60d87680c9ff2c1520b03ae602d4afdc332
Martin Arce <m.arce@joviva.de>
2025-04-03 23:14:50 +0200

    up

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -21,22 +21,22 @@ return {
 			end,
 			desc = "Flash Treesitter",
 		},
-		{
-			"EE",
-			mode = "o",
-			function()
-				require("flash").remote()
-			end,
-			desc = "Remote Flash",
-		},
-		{
-			"R",
-			mode = { "o", "x" },
-			function()
-				require("flash").treesitter_search()
-			end,
-			desc = "Treesitter Search",
-		},
+		-- {
+		-- 	"EE",
+		-- 	mode = "o",
+		-- 	function()
+		-- 		require("flash").remote()
+		-- 	end,
+		-- 	desc = "Remote Flash",
+		-- },
+		-- {
+		-- 	"R",
+		-- 	mode = { "o", "x" },
+		-- 	function()
+		-- 		require("flash").treesitter_search()
+		-- 	end,
+		-- 	desc = "Treesitter Search",
+		-- },
 		-- {
 		--   "<s-t>",
 		--   mode = { "c" },
