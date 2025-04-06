1ffc364491e904179ba2afc1c5fd9b3d44ac78e3
Martin Arce <m.arce@joviva.de>
2025-04-06 18:30:40 +0200

    up

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -194,7 +194,7 @@ return {
 			keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
 			keymap.set(
 				"n",
-				"<leader>ef",
+				"<M-e>",
 				"<cmd>NvimTreeFindFileToggle<CR>",
 				{ desc = "Toggle file explorer on current file" }
 			) -- toggle file explorer on current file
