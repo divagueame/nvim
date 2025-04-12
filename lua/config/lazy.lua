7e043cc93ab89ca4cec08c8f5ae64a368541b1ea
Martin Arce <m.arce@joviva.de>
2025-04-12 09:49:37 +0200

    up

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -8,7 +8,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
       { out, "WarningMsg" },
       { "\nPress any key to exit..." },
-    }, true, {})
+                  
+   }, true, {})   
     vim.fn.getchar()
     os.exit(1)
   end
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -107,6 +107,9 @@ vim.opt.splitbelow = true
 vim.opt.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
+
+
+
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 5
 
