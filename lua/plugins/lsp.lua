4e9eaee1333c1c3322d769a577f991030e7f6514
Martin Arce <m.arce@joviva.de>
2025-04-21 22:02:54 +0200

    hover

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -192,7 +192,7 @@ return {
 			bashls = {},
 			marksman = {},
 			-- clangd = {},
-			-- gopls = {},
+			gopls = {},
 			-- pyright = {},
 			-- rust_analyzer = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
